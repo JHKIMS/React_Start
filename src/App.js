b6ecd4833e0e1803195ea93adb740a3b9c54b82b
JHKIMS <wjdgns5131@gmail.com>
2022-07-17 08:38:54 +0900

    React-ToDo할일목록2-Arr로 값 넣기

--- src/App.js
+++ src/App.js
@@ -18,6 +18,19 @@ export default class App extends Component{
       textDecoration:"none"
     }
   }
+
+  todoData =[
+    {
+      id:"1",
+      title:"공부하기",
+      completed: true
+    },
+    {
+      id:"2",
+      title:"청소하기",
+      completed: false
+    }
+  ]
   render(){
     return(
       <div className="container">
@@ -26,11 +39,14 @@ export default class App extends Component{
             <h1>ToDo List</h1>
           </div>
 
-          <div style={this.getStyle()}>
+        {this.todoData.map((data) =>(
+          <div style={this.getStyle()} key={data.id}>
             <input type="checkbox" defaultChecked={false} />
-              공부하기
+              {data.title}
             <button style={this.btnStyle}>x</button>
           </div>
+        ))}
+
         </div>
       </div>
     )
